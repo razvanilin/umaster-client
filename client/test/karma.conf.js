0da70cb9b28e0ac7581694c9469b327921b228d6
Razvan Ilin <razvanilin@gmail.com>
2016-06-18 22:30:36 +0100

    replaced bootstrap with materialize - a lot of code changes to accommodate the change

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -22,9 +22,10 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/angular/angular.js',
       'bower_components/jquery/dist/jquery.js',
-      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+      'bower_components/angular/angular.js',
+      'bower_components/materialize/bin/materialize.js',
+      'bower_components/angular-materialize/src/angular-materialize.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/lodash/lodash.js',
